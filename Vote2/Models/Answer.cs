935287155173d807c6185d495db99e273d1e55e8
FadyIDS <117088496+Fady-B@users.noreply.github.com>
2024-05-25 20:47:55 +0300

    temp

--- Vote2/Models/Answer.cs
+++ Vote2/Models/Answer.cs
@@ -4,7 +4,7 @@
     {
         public Int64 Id { get; set; }
         public Int64 QuestionId { get; set; }
-        public string AnswerQestion { get; set; }
+        public string? AnswerQestion { get; set; }
         
 
     }
